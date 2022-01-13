d6ed7b286c02b203333e1b6b2fb2d1535bb0f28f
Derek M. Robertson <dmr.webdev@gmail.com>
2022-01-13 15:24:49 -0800

    Updated styles

--- src/components/Bios/Bios.js
+++ src/components/Bios/Bios.js
@@ -4,8 +4,11 @@ import dmrPixel from "../../assets/dmrPixel.png";
 import jpsPixel from "../../assets/jpsPixel.png";
 import github from "../../assets/github.svg";
 import email from "../../assets/email.svg";
+import resume from "../../assets/resume.svg";
 import globe from "../../assets/globe.svg";
 import HeaderBar from "../../common/HeaderBar";
+import jpsResume from "../../assets/jpsResume.pdf";
+
 import Link from "next/link";
 
 export default function Bios() {
@@ -13,37 +16,35 @@ export default function Bios() {
     return (
       <div
         key={profile.name}
-        className="max-w-lg text-white bg-slate-700 p-4 rounded-lg space-y-4 my-4 lg:mx-4"
+        className="max-w-lg flex flex-col justify-between text-white bg-slate-700 p-4 rounded-lg space-y-4 my-4 lg:mx-4"
       >
-        {/* Circle Pic */}
-        <div className="">
-          <div
-            className={`rounded-3xl
-         ${profile.color} p-3 `}
-          >
+        <div>
+          <div className="flex justify-center">
             <div
-              className="rounded-3xl
-         overflow-hidden bg-white flex justify-center items-center"
+              className={`rounded-full p-3 `}
+              style={{ backgroundColor: profile.color }}
             >
-              <Image
-                src={profile.img}
-                alt={profile.name}
-                quality={100}
-                className=""
-              />
+              <div className=" rounded-full overflow-hidden bg-white flex justify-center items-center">
+                <Image
+                  src={profile.img}
+                  alt={profile.name}
+                  quality={100}
+                  className={""}
+                  style={profile.customImgStyle}
+                />
+              </div>
             </div>
+          </div>
+
+          <div className="py-4">
+            <h2 className="text-3xl text-center">{profile.name}</h2>
+          </div>
+          <div className="">
+            <p>{profile.bio}</p>
           </div>
         </div>
 
-        <div className="pt-2">
-          <h2 className="text-3xl text-center">{profile.name}</h2>
-        </div>
-
-        <div className="">
-          <p>{profile.bio}</p>
-        </div>
-
-        <div className="flex justify-between items-center bg-white p-2 rounded-md bg-slate-500">
+        <div className="flex justify-between items-center bg-white px-2 py-3 rounded-md bg-slate-500">
           <div className="w-[40px] flex items-center">
             <Link href={profile.githubLink}>
               <a>
@@ -52,8 +53,21 @@ export default function Bios() {
             </Link>
           </div>
           <div className="w-[40px]  flex items-center">
-            <Image src={email} alt="Email" />
+            <Link href={`mailto:${profile.email}`}>
+              <a>
+                <Image src={email} alt="Email" />
+              </a>
+            </Link>
           </div>
+
+          <div className="w-[40px]  flex items-center">
+            <Link href={profile.resume}>
+              <a>
+                <Image src={resume} alt="resume" />
+              </a>
+            </Link>
+          </div>
+
           <div className="w-[40px]  flex items-center">
             <Link href={profile.portfolioLink}>
               <a className="hover:fill-sky-500">
@@ -69,10 +83,10 @@ export default function Bios() {
   return (
     <section className="w-full -mt-8">
       {/*  h-screen bg-circuit-flipped */}
-      <div className="max-w-screen-2xl mx-auto bg-slate-600 rounded-3xl relative z-10">
+      <div className="  max-w-screen-2xl mx-auto bg-slate-800 rounded-3xl relative z-10">
         <HeaderBar id="aboutUs" text="The Devs" />
-        <div className="w-full flex flex-wrap justify-center ">
-          <div className="w-full flex flex-wrap justify-center">
+        <div className="w-full flex flex-wrap justify-center pt-4 pb-20">
+          <div className="w-full flex flex-wrap justify-center ">
             {biosRecords.map((profile) => profileTile(profile))}
           </div>
         </div>
@@ -84,29 +98,25 @@ export default function Bios() {
 const biosRecords = [
   {
     name: "Derek Robertson",
-    bio: `Lorem ipsum I'm gay If you fail to follow distinct styles or you name
-          a component without a meaningful descriptor, all your efforts can be
-          wasted in seconds. On the other hand, the naming tasks If you’re used
-          to working with icon fonts, one thing you might enjoy about them is
-          how easy it is to change the color. You’re largely limited to a single
-          color with icon fonts in a way`,
+    bio: `Derek Robertson is a Full Stack Engineer raised and currently working from the SF Bay Area. Derek has had a lifelong curiosity of understanding how technologies work and develop that's grown to be a lucrative passion in software engineering. Derek's worked everything from electro-mechanical engineering related fields to optimization of the latest trending web stacks. With a scrupulous attention to detail, Derek can help make sure your next big project is a cut above the competition.  `,
     img: dmrPixel,
     githubLink: "https://github.com/dmrwebdev",
+    email: "dmr.webdev@gmail.com",
+    resume: "#",
     portfolioLink: "https://www.developwithderek.com/",
-    color: "bg-cyan-700",
+    customImgStyle: "top-[20px]",
+    color: "#0e7490",
   },
   {
     name: "Josef Saltz",
-    bio: `Lorem ipsum I'm gay If you fail to follow distinct styles or you name
-          a component without a meaningful descriptor, all your efforts can be
-          wasted in seconds. On the other hand, the naming tasks If you’re used
-          to working with icon fonts, one thing you might enjoy about them is
-          how easy it is to change the color. You’re largely limited to a single
-          color with icon fonts in a way`,
+    bio: `Josef Saltz is a Full Stack Engineer also hailing from the SF Bay Area. Having years of experience working technology positions for large-scale organizations in the public sector bundled with a lifelong pursuit of tinkering and hacking has led to a strong proficiency in debugging and understanding UX. Josef's written in everything from 32-bit Assembly to Python in MITx courses, which has led to an extremely versatile set of skills and experience to bring to your next big project.`,
     img: jpsPixel,
+    customImgStyle: { top: "20px" },
     githubLink: "https://github.com/JosefSaltz",
+    email: "jps.webdev@gmail.com",
+    resume: "#",
     portfolioLink: "https://www.josefsaltz.com/",
-    color: "bg-orange-700",
+    color: "#031e2f",
   },
 ];
 
