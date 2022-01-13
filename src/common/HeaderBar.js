01f6217ed707f95f19f94eac941a0d4c0155e4a2
Derek M. Robertson <dmr.webdev@gmail.com>
2022-01-13 15:24:26 -0800

    Componetized

--- src/common/HeaderBar.js
+++ src/common/HeaderBar.js
@@ -1,7 +1,7 @@
 export default function HeaderBar({ id, text }) {
   return (
-    <div>
-      <h2 id={id} className="text-center text-white py-6">
+    <div className="bg-slate-600 rounded-2xl">
+      <h2 id={id} className="text-center text-white p-6">
         {text}
       </h2>
       <div className="min-h-[15px] bg-[#ad315e]" />
